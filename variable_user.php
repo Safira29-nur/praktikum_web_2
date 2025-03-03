f7fb1a9a9b7a91b7b06c003efcb4bd28ed8801c3
Safira Nur Azkia <firaaasafira98@gmail.com>
2025-03-03 20:48:25 +0700

    Add files via upload

---
+++ variable_user.php
@@ -0,0 +1,10 @@
+<?php
+    //mendefinisikan variables
+    $nama = "jake";
+    $umur = 20;
+    $bb = 67;
+
+    echo "nama saya adallah : " .$nama;
+    echo "<br/>umur : " .$umur. "tahun" ;
+    echo "<br/>berat badan : " .$bb. "kg";
+?>
